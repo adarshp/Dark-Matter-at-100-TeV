6820d3926b31dd69f39923c3243a8b44990a97c1
Adarsh Pyarelal <adarsh@login3.cm.cluster>
2017-03-03 23:13:57 -0700

    update

--- event_generation.py
+++ event_generation.py
@@ -14,19 +14,35 @@ def copy_cards(signal):
         signal.copy_cards('Cards/run_cards/run_card.dat',
                            'Cards/pythia_cards/pythia_card.dat',
                            'Cards/delphes_cards/FCChh.tcl')
-        signal.copy_param_card()
-        sh.copy('Cards/delphes_cards/momentumResolutionVsP.tcl', signal.directory+'/Cards/')
-        sh.copy('Cards/delphes_cards/muonMomentumResolutionVsP.tcl', signal.directory+'/Cards/')
+        # sh.copy('Cards/delphes_cards/momentumResolutionVsP.tcl', signal.directory+'/Cards/')
+        # sh.copy('Cards/delphes_cards/muonMomentumResolutionVsP.tcl', signal.directory+'/Cards/')
         sh.copy('Cards/me5_configuration.txt', signal.directory+'/Cards/')
+
+def write_pbs_scripts(processes, parser, nruns):
+    for process in tqdm(processes, dynamic_ncols = True,
+                        desc = 'Writing PBS submit scripts'):
+        with open(parser.get('PBS Templates', 'generate_script'), 'r') as f:
+            string = f.read()
+        with open(process.directory+'/generate_events.pbs', 'w') as f:
+            f.write(string.format(jobname =process.index,
+                                  username = parser.get('Cluster', 'username'),
+                                  email = parser.get('Cluster', 'email'),
+                                  group_list = parser.get('Cluster', 'group_list'),
+                                  nruns = str(nruns),
+                                  cput = str(28*nruns),
+                                  walltime = str(1*nruns),
+                                  cwd = os.getcwd(),
+                                  mg5_process_dir = process.directory))
 
 def main():
     # mg5_path = '/extra/adarsh/Tools/mg5/'
-    # do_parallel(lambda x: x.create_directory(mg5_path),signals)
-    # do_parallel(copy_cards,signals)
+    # do_parallel(lambda x: x.create_directory(mg5_path),signals,28)
+    # do_parallel(copy_cards,signals, 2)
     # parser = SafeConfigParser()
     # parser.read('config.ini')
-    # do_parallel(lambda x: x.write_pbs_script(parser, 200), signals)
-    map(lambda x: x.generate_events(), tqdm(signals, desc = "submitting PBS jobs"))
+    # write_pbs_scripts(signals, parser, 1)
+    # do_parallel(lambda x: x.write_pbs_script(parser, 1), signals, 28)
+    map(lambda x: x.generate_events(), tqdm(signals[0:2], desc = "submitting PBS jobs"))
 
 if __name__ == '__main__':
     main()
