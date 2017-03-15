6dca661cccb175bf5172e8c1ecb59276b1dea7a3
Adarsh Pyarelal <adarsh@login2.cm.cluster>
2017-03-15 13:54:09 -0700

    added data cleaning feature

--- make_feature_arrays.py
+++ make_feature_arrays.py
@@ -16,9 +16,6 @@ from BDTClassifier import BDTClassifier
 
 def make_signal_feature_arrays():
     for signal in tqdm(signals):
-        directory = signal.directory+'/MakeFeatureArray/Build/'
-        sp.call(['cp','BackgroundFeatureArrays/Build/SampleAnalyzer/User/Analyzer/Analysis.cpp',
-             directory+'SampleAnalyzer/User/Analyzer/'])
         scriptName = signal.index+'.pbs'
         with open(scriptName,'w') as f:
             f.write('''\
@@ -26,28 +23,37 @@ def make_signal_feature_arrays():
 #PBS -M adarsh@email.arizona.edu
 #PBS -W group_list=shufang
 #PBS -N {proc_name}
-#PBS -q standard
-#PBS -l jobtype=serial
-#PBS -l select=1:ncpus=1:mem=2gb
-#PBS -l place=pack:shared
+#PBS -q windfall
+#PBS -l select=1:ncpus=1:mem=6gb:pcmem=6gb
+#PBS -l place=free:shared
 #PBS -l cput=5:0:0
 #PBS -l walltime=5:0:0
-module load root
 date
-cd /xdisk/adarsh/Dark-Matter-at-100-TeV/{directory}
-source setup.sh
-make
-rm -rf ../Output/*
-./analyze.sh
+cd /xdisk/adarsh/Dark-Matter-at-100-TeV/MakeFeatureArrays/Build
+./analyze.sh {proc_name}
 date
 exit 0
-'''.format(directory = directory, proc_name = signal.index))
+'''.format(proc_name = signal.index))
         sp.call(['qsub',scriptName],stdout=open(os.devnull,'w'))
         os.remove(scriptName)
+
+def data_cleaning(proc_name):
+    header = 'ptl1,ptl2,ptb1,ptb2,mll,mbb,mllbb,mR,mTR,MET,THT\n'
+    with open('MakeFeatureArrays/Output/'+proc_name+'/feature_array.txt','r+') as f:
+        lines = f.readlines()
+        if not lines[0].startswith('p'):
+            f.seek(0)
+            lines.insert(0,header)
+            f.writelines(lines)
+            f.truncate()
 
 def main():
     if sys.argv[1] == '--signals':
         make_signal_feature_arrays()
+    if sys.argv[1] == '--dc':
+        bgs = ['tt','tbW','bbWW']
+        signal_names = map(lambda x: x.index, signals)
+        map(data_cleaning,tqdm(signal_names+bgs))
 
 if __name__ == '__main__':
     main()
