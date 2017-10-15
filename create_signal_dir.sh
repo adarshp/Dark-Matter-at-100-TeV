4a17e5c550f938bc77c62a5bac14a27fa6978b58
Adarsh Pyarelal <adarsh.pyarelal@gmail.com>
2017-10-15 13:58:27 -0700

    Added script to create signal MG5 directory

---
+++ create_signal_dir.sh
@@ -0,0 +1,15 @@
+# Create background directories for the DM project
+
+dm=`pwd`
+target_dir='/extra/adarsh/'
+mg5='/home/u13/adarsh/MG5_aMC_v2_6_0/bin/mg5_aMC'
+
+cd $target_dir
+
+process='higgsino_NLSP_bino_LSP'
+rm -rf $process
+$mg5 $dm/Cards/mg5_proc_cards/$process'.dat'
+cp $dm/Cards/delphes_cards/momentumResolutionVsP.tcl $process/Cards/
+cp $dm/Cards/delphes_cards/muonMomentumResolutionVsP.tcl $process/Cards/
+cp $dm/Cards/delphes_cards/FCChh.tcl $process/Cards/delphes_card.dat
+cp $dm/Cards/run_cards/run_card.dat $process/Cards/run_card.dat
